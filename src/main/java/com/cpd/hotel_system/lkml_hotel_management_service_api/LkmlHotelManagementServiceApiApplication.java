0c173fc2d5ca8fa5c6147c6ff99e6061f7a14dd0
Tharidu Lakmal <lkml@192.168.1.13>
2025-08-09 10:39:32 +0530

    Initial commit

---
+++ src/main/java/com/cpd/hotel_system/lkml_hotel_management_service_api/LkmlHotelManagementServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_system.lkml_hotel_management_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkmlHotelManagementServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkmlHotelManagementServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpd/hotel_system/lkml_hotel_management_service_api/LkmlHotelManagementServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpd.hotel_system.lkml_hotel_management_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkmlHotelManagementServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
